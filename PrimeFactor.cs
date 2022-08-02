855209ae910d68d1204d77fcd1d877648b3173f6
Your Name <you@example.com>
2022-08-02 16:41:27 +0530

    [Megha] Add: Factors Of A Given Number Is Printed

---
+++ PrimeFactor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public static  class PrimeFactor
+    {
+         public static void FindFactor()
+        {
+             int a, b;
+             Console.WriteLine("Enter a positive integer: ");
+             a = int.Parse(Console.ReadLine());
+             for (b = 1; b <= a; b++)
+             {
+                if (a % b == 0)
+                {
+                    Console.Write(b + "  ");
+                }
+             }
+             Console.Write("are the factors of " + a + "\n");
+         }
+    }
+}
