8b44715a20c379722e6c519c273e6386cc720363
Your Name <you@example.com>
2022-08-02 16:58:43 +0530

    [Megha] Add:  Checking Even Odd Number Is Completed

---
+++ EvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public static class EvenOrOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            Console.WriteLine("Enter The number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is even number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is odd number");
+            }
+        }
+    }
+}
