e693135d2159f65690bb1c3c998daa4719620b02
Your Name <you@example.com>
2022-08-02 16:49:44 +0530

    [Megha] Add: Calculating Quotient And Remainder Is Completed

---
+++ QuotientAndRemainder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public static class QuotientAndRemainder
+    {
+        public static void CheckQuotientAndRemainder()
+        {
+            int dividend = 25, devisor = 5;
+
+            int quotient = dividend / devisor;
+            int remainder = dividend % devisor;
+
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("Remainder is " + remainder);
+        }
+    }
+}
+
