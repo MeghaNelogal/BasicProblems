a237b9f7a577909f9c585a7b68079dbcf1331360
Your Name <you@example.com>
2022-08-02 15:04:58 +0530

    [Megha] Add: Welcome To Basic Core Probelms

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BasicProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***Welome To Basic Core Programs***");
+        }
+    }
+}
