54fa315da9cb9d1ce8aed1fdd40ed0a2487187db
Your Name <you@example.com>
2022-08-02 15:22:33 +0530

    [Megha] Add:  Calculating Power Of Two Is Completed

---
+++ PowerOfTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public static class PowerOfTwo
+    {
+        public static void CheckPower()
+        {
+            Console.WriteLine("Enter the number-> ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine("2 to the power of {0} is -> " + Math.Pow(2, i), i);
+            }
+        }
+    }
+}
